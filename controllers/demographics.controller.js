d5710e5f8810526a88c06962b9b4e0047b9ac960
Afsin Ustundag <afsin@amida.com>
2019-06-14 11:51:20 -0400

    more cleanup

--- controllers/demographics.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const shared = require('./shared.js');
-
-exports.listDemographics = function listDemographics(req, res) {
-    req.models.demographics.listDemographics()
-        .then(result => res.status(200).json(result))
-        .catch(shared.handleError(req, res));
-};
